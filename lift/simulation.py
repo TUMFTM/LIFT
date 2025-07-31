c8f06e0a2104743e1780dca171b20c5ccdc26035
Brian Dietermann <brian.dietermann@tum.de>
2025-07-31 10:00:08 +0200

    minor improvements using numpy arrays

--- lift/simulation.py
+++ lift/simulation.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from time import time
@@ -13,21 +14,19 @@ EPS = 1E-8  # Small epsilon value for numerical stability in calculations
 class Block(ABC):
     dti: pd.DatetimeIndex
 
-    _dt: pd.Timedelta = field(init=False)
+    _idx: int = field(init=False)
 
     @property
     def freq_hours(self) -> float:
         return pd.Timedelta(self.dti.freq).total_seconds()/3600
 
     @property
-    def dt(self) -> pd.Timedelta:
-        return self._dt
+    def idx(self) -> int:
+        return self._idx
 
-    @dt.setter
-    def dt(self, value):
-        if not value in self.dti:
-            raise ValueError("dt must be a valid timedelta within the dti range")
-        self._dt = value
+    @idx.setter
+    def idx(self, value: int):
+        self._idx = value
 
 
 @dataclass
@@ -61,15 +60,16 @@ class SupplyBlock(Block):
 @dataclass
 class PVSource(SupplyBlock):
     pwr_kwp: float
-    log_spec: pd.Series = None
+    log_spec: np.ndarray[float] = None
 
     def __post_init__(self):
         if self.log_spec is None:
-            self.log_spec = pd.Series(index=self.dti, data=np.random.uniform(low=0, high=1, size=len(self.dti)))
+            self.log_spec = pd.Series(index=self.dti,
+                                      data=np.random.uniform(low=0, high=1, size=len(self.dti))).values
 
     @property
     def generation_max_kw(self) -> float:
-        return self.log_spec.at[self.dt] * self.pwr_kwp
+        return self.log_spec[self.idx] * self.pwr_kwp
 
     def satisfy_demand(self, demand_kw: float) -> float:
         # Return remaining power demand after PV generation (negative if excess generation)
@@ -110,7 +110,7 @@ class StationaryStorage(SupplyBlock):
         dsoc = -1 * pwr_ess * self.freq_hours / self.capacity_kwh  # Change in SOC based on power applied
         self.soc += dsoc
         if self.soc < (0 - EPS) or self.soc > (1 + EPS):
-            raise ValueError(f"SOC {self.soc} out of bounds after applying power {demand_kw} kW at {self.dt}.")
+            raise ValueError(f"SOC {self.soc} out of bounds after applying power {demand_kw} kW at {self.dti[self.idx]}.")
 
         # Return remaining power demand after storage (negative, if PV excess generation cannot be charged into storage)
         return demand_kw - pwr_ess
@@ -139,7 +139,7 @@ class GridConnection(SupplyBlock):
     def satisfy_demand(self, demand_kw: float):
         # Apply power to the grid connection, updating peak power and costs/revenue.
         if abs(demand_kw) > (self.pwr_max_kw + EPS):
-            raise ValueError(f"Demand {demand_kw} kW exceeds maximum power {self.pwr_max_kw} kW at {self.dt}.")
+            raise ValueError(f"Demand {demand_kw} kW exceeds maximum power {self.pwr_max_kw} kW at {self.dti[self.idx]}.")
 
         if demand_kw > 0:
             self.pwr_peak_kw = max(self.pwr_peak_kw, demand_kw)
@@ -150,21 +150,21 @@ class GridConnection(SupplyBlock):
 
 @dataclass
 class FixedDemand(DemandBlock):
-    log: pd.Series() = None
+    log: np.ndarray[float] = None
 
     def __post_init__(self):
         if self.log is None:
-            self.log = pd.Series(index=self.dti, data=np.random.uniform(low=0, high=10, size=len(self.dti)))
+            self.log = pd.Series(index=self.dti, data=np.random.uniform(low=0, high=10, size=len(self.dti))).values
 
     @property
     def demand_kw(self) -> float:
         # Return the demand in kW for the current dt.
-        return self.log.at[self.dt]
+        return self.log[self.idx]
 
 
 @dataclass
 class Fleet(DemandBlock):
-    fleet_units: dict[str, 'FleetUnit'] = None
+    fleet_units: dict[str, FleetUnit] = None
 
     pwr_lim_kw: float = 22.0
 
@@ -194,14 +194,16 @@ class FleetUnit(DemandBlock):
 
     capacity_kwh: float = 100
 
-    soc_track: dict[pd.Timestamp, float] = field(init=False,
-                                                 default_factory=dict)
+    soc_track: np.typing.NDArray[np.float64] = field(init=False)
 
     soc: float = field(init=False,
                        default=0.0)
 
     pwr_max_kw: float = field(init=False,
                               default=11.0)
+
+    def __post_init__(self):
+        self.soc_track = np.zeros(len(self.dti), dtype=np.float64)
 
     @property
     def priority_lvl(self) -> float:
@@ -218,7 +220,7 @@ class FleetUnit(DemandBlock):
         pwr_chg = min(self.demand_kw, pwr_available_kw)
         # update SOC based on charging power
         self.soc += pwr_chg * self.freq_hours / self.capacity_kwh
-        self.soc_track[self.dt] = self.soc
+        self.soc_track[self.idx] = self.soc
         # return the charging power applied to this unit
         return pwr_chg
 
@@ -226,7 +228,7 @@ class FleetUnit(DemandBlock):
 @dataclass
 class Simulation:
     dti: pd.DatetimeIndex
-    blocks_demand: dict[str, DemandBlock] = field(init=False)
+    blocks_demand: dict[str, DemandBlock | Fleet | FixedDemand] = field(init=False)
     blocks_supply: dict[str, SupplyBlock] = field(init=False)
 
     def __post_init__(self):
@@ -253,34 +255,33 @@ class Simulation:
 
     def simulate(self):
 
-        # ToDo: Improve speed by using the following shortcuts to avoid repeated lookups.
-        # blocks = self.blocks.values()
-        # blocks_supply = self.blocks_supply
-        # blocks_demand = self.blocks_demand
-        # fleet = blocks_demand['fleet']
-        # dem = blocks_demand['dem']
+        # Improve speed by using the following shortcuts to avoid repeated lookups
+        blocks = self.blocks.values()
+        blocks_supply = tuple(self.blocks_supply[k] for k in ('pv', 'ess', 'grid'))
+        blocks_demand = self.blocks_demand
+        fleet = blocks_demand['fleet']
+        dem = blocks_demand['dem']
 
         # Simulate the vehicle fleet over the given datetime index.
-        for dt in self.dti:
+        for idx in range(len(self.dti)):
             # pass time of current timestep to all blocks
-            for block in self.blocks.values():
-                block.dt = dt
+            for block in blocks:
+                block.idx = idx
 
             # calculate maximum power supply
-            pwr_supply_max_kw = sum([block.generation_max_kw for block in self.blocks_supply.values()])
+            pwr_supply_max_kw = sum(block.generation_max_kw for block in blocks_supply)
             # get the total demand from the fixed demand block
-            pwr_demand_kw = self.blocks_demand['dem'].demand_kw
+            pwr_demand_kw = dem.demand_kw
 
             # define Fleet charging power limit for dynamic load management
-            self.blocks_demand['fleet'].pwr_lim_kw = pwr_supply_max_kw - pwr_demand_kw
+            fleet.pwr_lim_kw = pwr_supply_max_kw - pwr_demand_kw
 
             # add fleet demand to the total demand
-            pwr_demand_kw += self.blocks_demand['fleet'].demand_kw
+            pwr_demand_kw += fleet.demand_kw
 
             # satisfy demand with supply blocks (order represents priority)
-            for name in ['pv', 'ess', 'grid']:
-                pwr_demand_kw = self.blocks_supply[name].satisfy_demand(demand_kw=pwr_demand_kw)
-
+            for block in blocks_supply:
+                pwr_demand_kw = block.satisfy_demand(demand_kw=pwr_demand_kw)
 
         pass
 
@@ -294,4 +295,4 @@ if __name__ == "__main__":
     sim.simulate()
 
     # stop time tracking
-    print(f"Simulation completed in {time() - start_time:.2f} seconds.")
+    print(f'Simulation completed in {time() - start_time:.2f} seconds.')
