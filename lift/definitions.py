1829e7575a6e9f953b3ee03ee87ede956ddd705d
Brian Dietermann <brian.dietermann@tum.de>
2025-09-12 14:57:35 +0200

    update yearly consumption default

--- lift/definitions.py
+++ lift/definitions.py
@@ -129,7 +129,7 @@ DEF_ENERGY_SYSTEM = DefinitionEnergySystem(
                                                     'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7',
                                                     'L0', 'L1', 'L2'],
                                            index=2),  # default value: 2 corresponds to G0
-    settings_dem_yr=SettingsSlider(min_value=0, max_value=1000, value=25, step=1, factor=1E6),
+    settings_dem_yr=SettingsSlider(min_value=0, max_value=1000, value=50, step=1, factor=1E6),
     settings_grid_preexisting=SettingsNumberInput(min_value=0, max_value=5000, value=200, factor=1E3),
     settings_grid_expansion=SettingsSlider(min_value=0, max_value=5000, value=0, step=1, factor=1E3),
     settings_pv_preexisting=SettingsNumberInput(min_value=0, max_value=1000, value=10, factor=1E3),
