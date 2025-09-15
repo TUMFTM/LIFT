f0f29d68ead1af6e98d543b04c4508266c4fe7fc
Brian Dietermann <brian.dietermann@tum.de>
2025-09-15 15:03:15 +0200

    change working directory to allow streamlit to find config.toml

--- lift/__main__.py
+++ lift/__main__.py
@@ -1,8 +1,12 @@
+import os
 from pathlib import Path
 import sys
 import subprocess
 
 
+# change working directory to package root; required for streamlit to find config.toml resources
+package_root = Path(__file__).parent
+os.chdir(package_root)
 
 def main():
     # get the absolute path to the frontend.py file in the 'lift' package
